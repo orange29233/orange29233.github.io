8f7f57137252f51da7d041b1c2e38e8b045034cd
orange29233 <orange29233@126.com>
2018-09-28 10:08:06 +0800

    增加TYPESCRIPT主页

---
+++ test_projects/element-demo/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <topheader></topheader>
+    <br>
+    <div class="updatemsg">
+        <p>{{updatemsg}}</p>
+    </div>
+    <img :src="imgpath" width="100%" height="404px">
+    <maincontent></maincontent>
+  </div>
+</template>
+
+<script>
+import topheader from "@/components/header/topheader";
+import maincontent from "@/components/maincontent/maincontent"
+export default {
+  name: "HelloWorld",
+  data(){
+    return{
+      updatemsg:"TypeScript 2.7 现已发布。现在下载我们最新版本！",
+      imgpath:"static/img/mmd.png"
+    }
+  },
+  components: {
+    topheader,
+    maincontent
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.updatemsg {
+  width: 100%;
+  height: 50px;
+  background: #dddddd;
+}
+.updatemsg p {
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
